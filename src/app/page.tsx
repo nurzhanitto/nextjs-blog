f77ba548238eb78b1e219c51ce0e2f0683e0764a
Nurzhanitto <n.ilyasov@jgarant.kz>
2025-03-03 18:26:14 +0500

    Added Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from "public/hero.png";
+import Hero from "../../public/hero.png";
 import Button from "../components/Button/Button";
 
 export default function Home() {
-  return (
-    <>
+    return (
       <div className={styles.container}>
         <div className={styles.item}>
           <h1 className={styles.title}>
@@ -15,11 +14,12 @@ export default function Home() {
             Turning your Idea into Reality. We bring together the teams from the
             global tech industry.
           </p>
-          <Button url="/portfolio" text="See Our Works" />
+          <Button url="/portfolio" text="See Our Works"/>
         </div>
-        <div className={styles.item}></div>
-        {/* <Image src={Hero} alt="" className={styles.img} /> */}
+        <div className={styles.item}>
+          <Image src={Hero} alt="" className={styles.img} />
+        </div>
       </div>
-    </>
-  );
-}
+    );
+  }
+  
